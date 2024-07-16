4e5d436178d741eb686b90f7f0cc0e39d589e791
Shreya Agarwal <a.shreya202@gmail.com>
2024-07-16 22:23:26 +0530

    feat: add check marks in the year view

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,11 @@
 // take data from local storage and make changes to the calendar UI
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     // we need a timeout because the target calendar elements are dynamically loaded and not even DOMContentLoaded event is fired
     this.setTimeout(() => {
-        chrome.storage.local.get('customCalendar')
+        chrome.storage.local.get("customCalendar")
         .then((currentValue) => {
-            currentValue = currentValue['customCalendar'];    
+            currentValue = currentValue["customCalendar"];    
             for (const [date, workouts] of Object.entries(currentValue)) {
                 console.log(`${date}: ${workouts}`);
 
@@ -17,21 +17,27 @@ window.addEventListener('load', () => {
                 const dayElementPattern = `a[href="/my/calendar/day/${date}"]`;
                 const dayElement = document.querySelector(dayElementPattern);
 
-                // add a "check mark" only if not present already for an FB workout
+                // add a "no-activity" type container only if not present
                 if (!dayElement.querySelector(".activity-icon")) {
                     const outerDiv = document.createElement("div");
                     outerDiv.classList.add("activity-icon");
                     outerDiv.classList.add("hover-hide");
                     outerDiv.innerHTML = [
-                        `<div class="icon-type workout-complete">`,
-                        `<i class="material-symbols-outlined fill" aria-hidden="true" style="background: linear-gradient(to bottom right, #3d97ce 0%, #666666 100%);-webkit-text-fill-color: transparent;background-clip: text">check_circle</i>`,
+                        `<div class="icon-type no-activity">`,
+                        `<i class="material-symbols-outlined fill" aria-hidden="true" style="background: linear-gradient(to bottom right, #3d97ce 0%, #666666 100%);-webkit-text-fill-color: transparent;background-clip: text">radio_button_unchecked</i>`,
                         `</div>`,
-                    ].join('');
+                    ].join("");
                     dayElement.querySelector(".date-header").insertAdjacentElement("afterend", outerDiv);
                 }
 
-                // create the workout container element only if not present already
-                if (!dayElement.querySelector(".schedule-slots")) {
+                // flip "no-activity" to "workout-complete"
+                const iconDiv = dayElement.querySelector(".icon-type");
+                iconDiv.classList.replace("no-activity", "workout-complete");
+                const iconElement = iconDiv.querySelector("i");
+                iconElement.innerHTML = "check_circle";
+
+                // create the workout container element only if not present already and not in year view
+                if (!dayElement.querySelector(".schedule-slots") && location.href.search("year") == -1) {
                     const outerDiv = document.createElement("div");
                     outerDiv.classList.add("schedule-slots");
 
